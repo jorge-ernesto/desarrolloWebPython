80a421339331367914e670cef9af1f0e49c7ecfe
Alexander Segovia <xander2497@gmail.com>
2022-11-08 21:05:44 -0500

    servidor Django

---
+++ primerProyecto/ejemplo_django/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+app_name = 'ejemplo_django'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('ingreso',views.ingreso,name='ingreso')
+]
