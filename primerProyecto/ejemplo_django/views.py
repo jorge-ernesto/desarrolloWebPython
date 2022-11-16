3fe6887d2b0911d3019d99d56bd09a5b0b5a430d
Alexander Segovia <xander2497@gmail.com>
2022-11-15 21:42:59 -0500

    Pre-parcial

--- primerProyecto/ejemplo_django/views.py
+++ primerProyecto/ejemplo_django/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import usuariosDjango
 
 # Create your views here.
-lista_elementos = [['Javier','Hilario','35'],['Dante','Arroyo','32'],['Luis','Velazco','33']]
-
 def index(request):
     return HttpResponse('Esta es mi primera vista')
 
 def ingreso(request):
+    usuariosInformacion = usuariosDjango.objects.all().order_by('id')
     if request.method == 'POST':
-        nuevoUsuario = []
-        print('Hola, se ha posteado informacion')
-        nombre = request.POST.get('nombreUsuario')
-        apellido = request.POST.get('apellidoUsuario')
-        edad = request.POST.get('edadUsuario')
-        print('El nombre del usuario es : ' + str(nombre))
-        print('El apellido del usuario es : ' + str(apellido))
-        print('La edad del usuario es : ' + str(edad))
-        nuevoUsuario.append(str(nombre))
-        nuevoUsuario.append(str(apellido))
-        nuevoUsuario.append(str(edad))
-        lista_elementos.append(nuevoUsuario)
+        if 'Crear' in request.POST:
+            nuevoUsuario = []
+            print('Hola, se ha posteado informacion')
+            nombre = request.POST.get('nombreUsuario')
+            apellido = request.POST.get('apellidoUsuario')
+            edad = request.POST.get('edadUsuario')
+            print('El nombre del usuario es : ' + str(nombre))
+            print('El apellido del usuario es : ' + str(apellido))
+            print('La edad del usuario es : ' + str(edad))
+            nuevoUsuario.append(str(nombre))
+            nuevoUsuario.append(str(apellido))
+            nuevoUsuario.append(str(edad))
+            usuariosDjango(nombre=str(nombre),apellido=str(apellido),edad=str(edad)).save()
+            usuariosInformacion = usuariosDjango.objects.all().order_by('id')
+        elif 'Filtrar' in request.POST:
+            filtradoEdad = request.POST.get('edadFiltrado')
+            usuariosInformacion = usuariosDjango.objects.filter(edad=filtradoEdad)
     return render(request,'ejemplo_django/ingreso.html',{
-        'usuarios':lista_elementos,
+        'usuarios':usuariosInformacion,
     })
+
+#Wappalyzer
