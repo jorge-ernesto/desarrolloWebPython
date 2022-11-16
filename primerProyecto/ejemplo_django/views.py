c081e57a7b00a2b9c898edd8a7e2bb016bdd7baf
Alexander Segovia <xander2497@gmail.com>
2022-11-15 19:14:16 -0500

    registroUsuario app

--- primerProyecto/ejemplo_django/views.py
+++ primerProyecto/ejemplo_django/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+lista_elementos = [['Javier','Hilario','35'],['Dante','Arroyo','32'],['Luis','Velazco','33']]
 
 def index(request):
     return HttpResponse('Esta es mi primera vista')
 
 def ingreso(request):
-    lista_elementos = [['Javier','Hilario','35'],['Dante','Arroyo','32'],['Luis','Velazco','33']]
+    if request.method == 'POST':
+        nuevoUsuario = []
+        print('Hola, se ha posteado informacion')
+        nombre = request.POST.get('nombreUsuario')
+        apellido = request.POST.get('apellidoUsuario')
+        edad = request.POST.get('edadUsuario')
+        print('El nombre del usuario es : ' + str(nombre))
+        print('El apellido del usuario es : ' + str(apellido))
+        print('La edad del usuario es : ' + str(edad))
+        nuevoUsuario.append(str(nombre))
+        nuevoUsuario.append(str(apellido))
+        nuevoUsuario.append(str(edad))
+        lista_elementos.append(nuevoUsuario)
     return render(request,'ejemplo_django/ingreso.html',{
         'usuarios':lista_elementos,
     })
